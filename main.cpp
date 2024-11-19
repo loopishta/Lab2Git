bc75f216d868935c62341f72ac1da70be5c40b53
Eremina <vikto.eremina@yandex.ru>
2024-11-19 23:15:04 +0500

    Добавлена третья функция

--- main.cpp
+++ main.cpp
@@ -36,10 +36,35 @@ void f2()
     }
 }
 
+void f3()
+{
+    string a,b,c;
+    vector <string> array(3);
+    ifstream in("txt.txt");
+    ofstream out;
+    out.open("out_txt.txt");
+
+    if (in.is_open())
+    {
+        in >> a >> b >> c;
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+    }
+        if (out.is_open())
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            out << array[i] << " ";
+        }
+    }
+}
+
 int main()
 {
     f1();
     f2();
+    f3();
 }
 
 
