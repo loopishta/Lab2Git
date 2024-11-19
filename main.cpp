a3258f0262b61a3520b80a2cdb4253d51c6a903c
Eremina <vikto.eremina@yandex.ru>
2024-11-19 23:00:08 +0500

    Добавлена вторая функция

--- main.cpp
+++ main.cpp
@@ -17,13 +17,29 @@ void f1()
         array[0] = a;
         array[1] = b;
         array[2] = c;
-        #cout << array[0] << " " << array[1]<< " " << array[2] << " "<< endl;
+
+    }
+}
+
+void f2()
+{
+    string a,b,c;
+    vector <string> array(3);
+    ifstream in("txt.txt");
+    if (in.is_open())
+    {
+        in >> a >> b >> c;
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        cout << array[0] << " " << array[1]<< " " << array[2] << " "<< endl;
     }
 }
 
 int main()
 {
     f1();
+    f2();
 }
 
 
