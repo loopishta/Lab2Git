803d228e471bbaef814787e65d0045af85b16253
Eremina <vikto.eremina@yandex.ru>
2024-11-19 22:52:36 +0500

    Добавлена первая функция

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+void f1()
+{
+    string a,b,c;
+    vector <string> array(3);
+    ifstream in("txt.txt");
+    if (in.is_open())
+    {
+        in >> a >> b >> c;
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        #cout << array[0] << " " << array[1]<< " " << array[2] << " "<< endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    f1();
 }
+
+
+
